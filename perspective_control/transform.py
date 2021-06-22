eeae7b83e7fdfa338e3024018ca090c74a5cbfd8
jointpoints <jointpoints@ya.ru>
2021-06-22 21:36:55 +0300

    Minor fixes and new samples
    1. New comments and transformations have been added.
    2. New image samples have been collected.

--- perspective_control/transform.py
+++ perspective_control/transform.py
@@ -8,7 +8,7 @@ __author__ = 'Andrei Eliseev'
 import perspective_control.verbosity
 from skimage import transform
 import numpy as np
-from math import cos, sin, asin
+from math import cos, sin, asin, tan
 
 
 
@@ -18,7 +18,7 @@ def remove_perspective(image, v_h, v_v, verbosity):
 	verbosity_label_capacity = 51
 	# 1. Estimate focal length
 	perspective_control.verbosity.start_progress(verbosity, 'Removing perspective', verbosity_label_capacity)
-	focal_length = 1#int((image.shape[0]**2 + image.shape[1]**2)**.5)
+	focal_length = int((image.shape[0]**2 + image.shape[1]**2)**.5)
 
 	# 2. Estimate rotation angles
 	# 2.1. Find radius-vectors of vanishing points in 3D
@@ -32,22 +32,25 @@ def remove_perspective(image, v_h, v_v, verbosity):
 	t_x = v_v[1]
 	phi_x = asin(cos_x)
 	# 2.4. We need v_h_3d to be a radius-vector of v_h after rotation about X axis.
-	#      Let us apply the inverse rotation to see where the initial vector must be
-	inverse_spin_x = np.array([[ cos(-phi_x), 0, sin(-phi_x)],
-	                           [      0     , 1,      0     ],
-	                           [-sin(-phi_x), 0, cos(-phi_x)]])
-	v_h_3d_original = inverse_spin_x @ v_h_3d
+	#      Let us apply the rotation to find out the Y-shift to apply after spin.
+	spin_x = np.array([[ cos(phi_x), 0, sin(phi_x)],
+	                   [     0     , 1,     0     ],
+	                   [-sin(phi_x), 0, cos(phi_x)]])
 	# 2.5. Specify t_y and phi_y
+	t_y_1 = v_h[0]
+	t_y_2 = -focal_length * tan(phi_x)
+	v_h_3d = spin_x @ v_h_3d
+	v_h_3d[0] = 0
+	v_h_3d_n = v_h_3d / np.linalg.norm(v_h_3d)
 	cos_y = v_h_3d_n[2]
-	t_y = v_h[0] + int(v_h_3d_original[0])
 	phi_y = asin(cos_y) if v_h[1] > v_v[1] else -asin(cos_y)
 	perspective_control.verbosity.update_progress(verbosity, 7)
 
 	# 3. Apply rotation
 	# 3.1. Shift the origin of the image
 	#      [3 x 3]
-	origin_shift = np.array([[1, 0, -t_x],
-	                         [0, 1, -t_y],
+	origin_shift = np.array([[1, 0, - t_x],
+	                         [0, 1, -t_y_1],
 	                         [0, 0,   1 ]])
 	# 3.2. Go from uniform 2D to uniform 3D: (x, y, 1) -> (x, y, focal length, 1)
 	#      [4 x 3]
@@ -61,25 +64,31 @@ def remove_perspective(image, v_h, v_v, verbosity):
 	                   [0, cos(phi_x), -sin(phi_x), 0],
 	                   [0, sin(phi_x),  cos(phi_x), 0],
 	                   [0,     0     ,      0     , 1]])
-	# 3.4. Spin the world about the Y axis by phi_y radians
+	# 3.4. Shift the image so that v_h has X coordinate zero
+	#      [4 x 4]
+	add_y = np.array([[1, 0, 0,    0  ],
+	                  [0, 1, 0, -t_y_2],
+	                  [0, 0, 1,    0  ],
+	                  [0, 0, 0,    1  ]])
+	# 3.5. Spin the world about the Y axis by phi_y radians
 	#      [4 x 4]
 	spin_y = np.array([[ cos(phi_y), 0, sin(phi_y), 0],
 	                   [     0     , 1,     0     , 0],
 	                   [-sin(phi_y), 0, cos(phi_y), 0],
 	                   [     0     , 0,     0     , 1]])
-	# 3.5. Go from uniform 3D to uniform 2D: (x, y, z, 1) -> (x, y, 1)
+	# 3.6. Go from uniform 3D to uniform 2D: (x, y, z, 1) -> (x, y, 1)
 	#      [3 x 4]
 	dim_shift_3to2 = np.array([[1, 0,        0        , 0],
 	                           [0, 1,        0        , 0],
 	                           [0, 0, focal_length**-1, 0]])
-	# 3.6. Shift the origin back
+	# 3.7. Shift the origin back
 	#      [3 x 3]
-	inv_origin_shift = np.array([[1, 0, t_x],
-	                             [0, 1, t_y],
-	                             [0, 0,  1 ]])
-	# 3.7. Resulting rotation transform
-	#      [3 x 3] x [3 x 4] x [4 x 4] x [4 x 4] x [4 x 3] x [3 x 3] = [3 x 3]
-	transform_matrix = inv_origin_shift @ dim_shift_3to2 @ spin_y @ spin_x @ dim_shift_2to3 @ origin_shift
+	inv_origin_shift = np.array([[1, 0,  t_x ],
+	                             [0, 1, t_y_1],
+	                             [0, 0,   1  ]])
+	# 3.8. Resulting rotation transform
+	#      [3 x 3] x [3 x 4] x [4 x 4] x [4 x 4] x [4 x 4] x [4 x 3] x [3 x 3] = [3 x 3]
+	transform_matrix = inv_origin_shift @ dim_shift_3to2 @ spin_y @ add_y @ spin_x @ dim_shift_2to3 @ origin_shift
 	major_transform = transform.EuclideanTransform(matrix=transform_matrix)
 	perspective_control.verbosity.update_progress(verbosity, 7)
 
